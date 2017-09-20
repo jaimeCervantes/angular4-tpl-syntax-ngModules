d80f12a53e3eddf8b44ae0fba3e8b0f0eb0810ee
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-09-20 15:58:44 -0500

    Adding attribute, class and style binding explanation

--- src/app/fund-attr-class-style-binding/fund-attr-class-style-binding.component.ts
+++ src/app/fund-attr-class-style-binding/fund-attr-class-style-binding.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'fund-attr-class-style-binding',
-  templateUrl: './fund-attr-class-style-binding.component.html'
+  templateUrl: './fund-attr-class-style-binding.component.html',
+  styles: [
+    '.red { color: red}'
+  ]
 })
 
-export class FundAttrClassStyleBindingComponent {}
+export class FundAttrClassStyleBindingComponent {
+  private actionName = 'Enviar';
+  private classBinding = 'tres dos uno';
+  private isSpecial = 'is-special';
+}
