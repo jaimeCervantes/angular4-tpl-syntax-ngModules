9031b2add68768cfffb6504fca55b4f6b42f422b
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-09-23 19:52:48 -0500

    Adding template expression operators section

--- src/app/fund-tpl-expr-operators/fund-tpl-expr-operators.component.ts
+++ src/app/fund-tpl-expr-operators/fund-tpl-expr-operators.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: 'fund-tpl-expr-operators.component.html'
 })
 
-export class FundTplExprOperatorsComponent {}
+export class FundTplExprOperatorsComponent {
+  title = 'Titulo de prueba para pipes';
+  person = {
+    nombre: 'Jaime',
+    edad: 29,
+    fechaNacimiento: '1987-10-28T08:00:00.000Z',
+    hijos: [{
+      nombre: 'Jaimito',
+      edad: '9 meses',
+      fechaNacimiento: '2016-12-27T08:00:00.000Z',
+    }]
+  };
+
+}
