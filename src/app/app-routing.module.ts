f8b8804d0aadb9a331e3535f4132f1862a2904eb
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-09-26 11:36:34 -0500

    deleting comments and changing directory names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { TplSyntaxComponent } from './tpl-data-binding/tpl-syntax/tpl-syntax.component';
 
 const routes: Routes = [
-  // redirect so url path is correct
-  { path: '', redirectTo: 'template-syntax', pathMatch: 'full' }
+  // { path: '', redirectTo: 'tpl-syntax-data-binding', pathMatch: 'full' },
+  { path: 'tpl-syntax-data-binding', loadChildren: 'app/tpl-data-binding/tpl-syntax-data-binding.module#TplSyntaxDataBindingModule'}
 ];
 
 @NgModule({
--- src/app/tpl-data-binding/tpl-syntax-data-binding-routing.module.ts
+++ src/app/tpl-data-binding/tpl-syntax-data-binding-routing.module.ts
@@ -12,16 +12,17 @@ import { InputOutputPropsComponent } from './input-output-props/input-output-pro
 import { TplExprOperatorsComponent } from './tpl-expr-operators/tpl-expr-operators.component';
 
 const routes: Routes = [
-  { path: 'template-syntax', component: TplSyntaxComponent },
-  { path: 'template-syntax/property-binding', component: PropertyBindingComponent },
-  { path: 'template-syntax/attr-class-style-binding', component: AttrClassStyleBindingComponent },
-  { path: 'template-syntax/event-binding', component: EventBindingComponent },
-  { path: 'template-syntax/two-way-binding', component: TwoWayBindingComponent },
-  { path: 'template-syntax/built-in-attr-directives', component: BuiltInAttrDirectivesComponent },
-  { path: 'template-syntax/built-in-struct-directives', component: BuiltInStructDirectivesComponent },
-  { path: 'template-syntax/template-reference-variables', component: TplReferenceVariablesComponent },
-  { path: 'template-syntax/input-output-props', component: InputOutputPropsComponent },
-  { path: 'template-syntax/tpl-expr-operators', component: TplExprOperatorsComponent }
+  // { path: '', redirectTo: 'tpl-syntax-data-binding', pathMatch: 'full' },
+  { path: 'tpl-syntax-data-binding/tpl-syntax', component: TplSyntaxComponent },
+  { path: 'tpl-syntax-data-binding/property-binding', component: PropertyBindingComponent },
+  { path: 'tpl-syntax-data-binding/attr-class-style-binding', component: AttrClassStyleBindingComponent },
+  { path: 'tpl-syntax-data-binding/event-binding', component: EventBindingComponent },
+  { path: 'tpl-syntax-data-binding/two-way-binding', component: TwoWayBindingComponent },
+  { path: 'tpl-syntax-data-binding/built-in-attr-directives', component: BuiltInAttrDirectivesComponent },
+  { path: 'tpl-syntax-data-binding/built-in-struct-directives', component: BuiltInStructDirectivesComponent },
+  { path: 'tpl-syntax-data-binding/template-reference-variables', component: TplReferenceVariablesComponent },
+  { path: 'tpl-syntax-data-binding/input-output-props', component: InputOutputPropsComponent },
+  { path: 'tpl-syntax-data-binding/tpl-expr-operators', component: TplExprOperatorsComponent }
 ];
 
 @NgModule({
