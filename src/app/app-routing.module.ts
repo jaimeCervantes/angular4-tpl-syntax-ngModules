bfe8091e5ed6c10095245939e97b2b775086c42b
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-09-26 13:03:33 -0500

    Deleting useless import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TplSyntaxComponent } from './tpl-data-binding/tpl-syntax/tpl-syntax.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'tpl-syntax-data-binding', pathMatch: 'full' },
